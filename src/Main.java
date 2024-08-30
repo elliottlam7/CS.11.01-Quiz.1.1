3a52c06b949dcaff6a43531c06c290bf5d388983
elliottlam <elliottla2027@student.cis.edu.hk>
2024-08-30 12:09:04 +0800

    quiz 1 done

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 *****************************************************************************************************
 * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
 * “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
+* NAME: Elliott Lam
 *****************************************************************************************************
 */
 
@@ -12,8 +12,19 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Please enter your age: ");
+        int age = scanner.nextInt();
 
+        scanner.nextLine();
+        System.out.println("Please enter your first name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Please enter your favourite food: ");
+        String favouriteFood = scanner.nextLine();
+
+        System.out.println("First Name: " + firstName + "\nAge: " + age + "\nFavourite Food: " +favouriteFood);
 
     }
 
